17d9e9fff0a8b6bdd009a42c41eaa8c98c382985
Rich <richard.g.otap@gmail.com>
2023-02-22 22:36:15 -0800

    bracket-bevy: Add rex drawing support.

--- bracket-bevy/src/lib.rs
+++ bracket-bevy/src/lib.rs
@@ -10,15 +10,19 @@ use consoles::*;
 mod random_resource;
 pub use consoles::{DrawBatch, VirtualConsole};
 pub use random_resource::*;
+pub mod rex;
 mod textblock;
 
 pub type FontCharType = u16;
 
 pub mod prelude {
+    pub use crate::rex;
+    pub use crate::rex::*;
     pub use crate::{
         consoles::TextAlign, cp437::*, textblock::*, BTermBuilder, BracketContext, DrawBatch,
         RandomNumbers, TerminalScalingMode, VirtualConsole,
     };
     pub use bracket_color::prelude::*;
     pub use bracket_geometry::prelude::*;
+    pub use bracket_rex::prelude::*;
 }
